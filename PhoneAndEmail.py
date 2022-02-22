c0ae3e3a41b9315dda68f09d3d9967ae0b3a4ea8
YRAL8 <nebrask12xx@gmail.com>
2022-02-22 10:12:27 +0200

    Add PhoneAndEmail.py

--- PhoneAndEmail.py
+++ PhoneAndEmail.py
@@ -11,6 +11,7 @@ phoneRegex = re.compile(r'''(
         (\d{4})              #последние 4 цифры
         (\s*(ext|x|ext.)\s*(\d{2,5}))?
 )''',re.VERBOSE)
+
 # Создание регулярного выражения  для адрессов почты
 emailRegex = re.compile(r'''(
             [a-zA-Z0-9._%+-]+       #имя пльзователя
@@ -18,6 +19,22 @@ emailRegex = re.compile(r'''(
             [a-zA-Z0-9.-]+          #тмя домена
             (\.[a-zA-Z] {2,4})      #остальная часть адресса 
 )''',re.VERBOSE)
-#TODO: Найти соответствия в тексте содержащемся в буфере обмена
+#Найти соответствия в тексте содержащемся в буфере обмена
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+#Скопировать результат в буфер обмена
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Скопированно в буфер обмена:')
+    print('\n'.join(matches))
+else:
+    print('Телефонные номера и адресса почт не обнаружены.')
 
-#TODO: Скопировать результат в буфер обмена
+
